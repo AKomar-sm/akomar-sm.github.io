9870a6830d7beca939e18b9421e9d6534222ce83
a_komar <a.komar@soffteco.com>
2023-06-17 14:11:01 +0300

    Added top block

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Counter from './components/Counter/Counter';
-import { useAppDispatch, useAppSelector } from './redux/hooks';
-import { counterActions } from './redux/counter/slice';
+import doorOpened from './assets/images/doorOpened.jpg';
+import arrowButton from './assets/images/arrowButton.png';
+import './App.scss';
 
 function App(): JSX.Element {
-  const dispatch = useAppDispatch();
-
-  const { value } = useAppSelector((state) => state.counter);
-
-  const increment = (): void => {
-    dispatch(counterActions.increment());
-  };
-
-  const decrement = (): void => {
-    dispatch(counterActions.decrement());
-  };
-
-  const incrementAsync = (): void => {
-    dispatch(counterActions.incrementAsync());
-  };
-
-  const decrementAsync = (): void => {
-    dispatch(counterActions.decrementAsync());
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter
-          onIncrement={increment}
-          onDecrement={decrement}
-          onIncrementAsync={incrementAsync}
-          onDecrementAsync={decrementAsync}
-          value={value}
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="topLogoWrapper">
+        <div className="topWrapperLeftSide">
+          <p className="topVastuWord">VASTU</p>
+          <img
+            src={doorOpened}
+            className="doorOpenedImg"
+            alt="doorOpenedImg"
+            width="700px"
+            height="700px"
+          />
+        </div>
+        <div className="topWrapperRightSide">
+          <p className="topSpaceWord">SPACE</p>
+          <div className="rightBottomSloganWrapper">
+            <p className="sloganText">Создаем пространства меняющие сознание</p>
+            <button className="freeConsultingButton">
+              <img src={arrowButton} alt="" />
+              <span>Бесплатная консультация</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
