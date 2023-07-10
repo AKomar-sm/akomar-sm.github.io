396fb02a4bb962a6776f3f02a5a90eccd993b910
a_komar <a.komar@soffteco.com>
2023-07-10 11:08:42 +0300

    сщккусеув иет рфтвдукы

--- src/sections/PortfolioSection/ArrowOnlyButton/ArrowOnlyButton.tsx
+++ src/sections/PortfolioSection/ArrowOnlyButton/ArrowOnlyButton.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { TfiArrowRight } from 'react-icons/tfi';
 
 import './arrow_only_button.scss';
+import { TODO_TYPE } from 'types';
 
 export const ArrowOnlyButton = ({ uid }: { uid: string }): JSX.Element => {
+  const onClick = (event: TODO_TYPE): TODO_TYPE => {
+    console.log('btn click! RJS');
+    window.parent.postMessage(uid + JSON.stringify(event), '*');
+  };
+
   return (
-    <button className="arrowOnlyBtn" onClick={() => ({})} id={uid}>
+    <button className="arrowOnlyBtn" onClick={onClick} id={uid}>
       <div className="buttomLeftLine" />
       <TfiArrowRight color="#d8a27d" fontSize="60px" />
     </button>
